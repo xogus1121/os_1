975e8f5b2608aa1528575dd6a25b9851b43822fd
zetyx <shwarzes89@gmail.com>
2013-10-26 16:11:54 +0900

    refactoring on queue

--- ready_queue.c
+++ ready_queue.c
@@ -1,3 +1,20 @@
 #include "ready_queue.h"
 
 
+void 
+enqueue_task(TASK_RQ *rq, PCB* task )
+{
+
+}
+
+bool
+dequeue_task(TASK_RQ *rq, PCB * task )
+{
+
+}
+
+bool
+is_empty() 
+{
+
+}
--- ready_queue.h
+++ ready_queue.h
@@ -1,8 +1,13 @@
 #include "process_structure.h"
 
-void enqueue_task(PCB *task);
-bool dequeue_task(PCB *task);
-bool is_empty();
-int get_queue_size();
-PCB *get_first_task();
-PCB *get_last_task();
+typedef st_ready_queue {
+	PCB *front, *rear;
+}TASK_RQ	;
+
+
+void enqueue_task(TASK_RQ *, PCB *);
+bool dequeue_task(TASK_RQ *, PCB *task);
+bool is_empty(TASK_RQ *);
+int get_queue_size(TASK_RQ *);
+PCB *get_first_task(TASK_RQ *);
+PCB *get_last_task(TASK_RQ *);
